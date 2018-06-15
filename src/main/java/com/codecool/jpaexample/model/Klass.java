4490bbd2734dad7aa7d743129795dd5e70d3fa3a
Dóra Surmann <surmann.dora@gmail.com>
2018-06-15 14:13:37 +0200

    enum CClLocation added to class fields & constructor

--- src/main/java/com/codecool/jpaexample/model/Klass.java
+++ src/main/java/com/codecool/jpaexample/model/Klass.java
@@ -1,5 +1,7 @@
 package com.codecool.jpaexample.model;
 
+import com.sun.deploy.security.ValidationState;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
@@ -14,8 +16,9 @@ public class Klass {
 
     public Klass() {}
 
-    public Klass(String name) {
+    public Klass(String name, CCLocation location) {
         this.name = name;
+        this.location = location;
     }
 
     public String getName() {
@@ -38,4 +41,13 @@ public class Klass {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Enumerated(value = EnumType.STRING)
+    private CCLocation location;
+
 }
+
+/* Create a new enumerated class called CCLocation with values MISKOLC, BUDAPEST,
+and KRAKOW! Add it as an attribute to Klass and set it in its constructor!
+How does it get represented in the database? Use the @Enumerated annotation to
+change this default behaviour!
+ */
